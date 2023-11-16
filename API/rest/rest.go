05a1661fedb65e94c8323a5eaafd6762cf89d31c
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2023-11-16 12:24:11 -0500

    beggining implementation of comment functionality

--- API/rest/rest.go
+++ API/rest/rest.go
@@ -14,12 +14,14 @@ func API(){
 	router.GET("/account", GetAccounts)
 	router.GET("/posts", GetPosts)
 	router.GET("/posts/fullcontext", GetPostsFullContext)
+	router.GET("/posts/:id/comments", GetPostsComments)
 
 	/*
 	*=========================POST METHODS================================
 	*/
 	router.POST("/account/new", NewAccount)
 	router.POST("/posts/create", NewPost)
+	router.POST("/posts/:id/comment", NewComment)
 	router.POST("/login",Login)
 
 	/*
@@ -27,12 +29,14 @@ func API(){
 	*/
 	router.PATCH("/account/update", UpdateAcc)
 	router.PATCH("/posts/update",UpdatePost)
+	router.PATCH("/posts/:id/comment", UpdateComment)
 
 	/*
 	*=========================DELETE METHODS================================
 	*/
 	router.DELETE("/account/delete/:id", DelAccount)
 	router.DELETE("/posts/delete/:id", DelPost)
+	router.DELETE("/posts/:id/delete/:id", DelComment)
 
 
 	//*activate the server
