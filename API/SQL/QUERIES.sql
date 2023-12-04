c7be374c570a1a0f7d8a143a2a70f5c6612ec513
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2023-12-03 21:54:06 -0500

    Update QUERIES.sql

--- API/SQL/QUERIES.sql
+++ API/SQL/QUERIES.sql
@@ -1,1 +1,35 @@
-SELECT * FROM ACCOUNTS
+-- Disable foreign key checks (if necessary)
+SET foreign_key_checks = 0;
+
+-- Delete all rows from the tables
+DELETE FROM ACCOUNTS;
+DELETE FROM IMAGES;
+DELETE FROM COMMENTS;
+DELETE FROM POSTS;
+
+-- Reset the seed for auto-incrementing columns
+ALTER TABLE ACCOUNTS AUTO_INCREMENT = 1;
+ALTER TABLE IMAGES AUTO_INCREMENT = 1;
+ALTER TABLE COMMENTS AUTO_INCREMENT = 1;
+ALTER TABLE POSTS AUTO_INCREMENT = 1;
+
+-- Enable foreign key checks (if disabled)
+SET foreign_key_checks = 1;
+
+INSERT INTO ACCOUNTS (fname, lname, fullname, email, pwd, pnum, username, accesslvl)
+VALUES(
+'guest',
+'guest',
+'guest guest',
+'guest@gmail.com',
+'guest',
+0000000000,
+'guest',
+)
+
+INSERT INTO IMAGES(imgname, size, date)
+VALUES(
+'img1.png',
+'0',
+CURDATE()
+)
